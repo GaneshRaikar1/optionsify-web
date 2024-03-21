692bbf4141aa73223dc6785a2177c5ea256d3290
Ganesh Raikar <ratedrnagesh28@gmail.com>
2024-03-21 17:15:13 +0530

    error solved

--- src/components/homeTwo/FeaturesArea.js
+++ src/components/homeTwo/FeaturesArea.js
@@ -9,7 +9,7 @@ const FeaturesArea = ({ features }) => {
                <div className="row">
                   <div className="col-xxl-11 offset-xxl-1">
                      <div className="section__title-wrapper mb-65">
-                        <p className="section__title">Optionsify platform is packed with features designed to make your trading experience as easy and efficient as possible. With our proprietary options trader, you can execute, monitor, and seamlessly trade. Whether you're a seasoned trader or just starting out, we've got you covered.</p>
+                        <p className="section__title">Optionsify platform is packed with features designed to make your trading experience as easy and efficient as possible. With our proprietary options trader, you can execute, monitor, and seamlessly trade. Whether you&aposre a seasoned trader or just starting out, we&aposve got you covered.</p>
                      </div>
                   </div>
                </div>
--- src/components/homeTwo/Services.js
+++ src/components/homeTwo/Services.js
@@ -11,20 +11,20 @@ const Services = () => {
                   <div className="col-xxl-12">
                      <div className="section__title-wrapper mb-50 text-center">
                         <h3 className="section__title">Empower Your Trading Journey with Optionsify</h3>
-                        <p>Optionsify is your partner in transitioning from discretionary to systematic trading, offering a comprehensive suite of products and services designed to elevate your trading success. With our advanced tools and expert guidance, you can unlock the full potential of your trading strategies and achieve consistent results in today's dynamic markets.</p>
+                        <p>Optionsify is your partner in transitioning from discretionary to systematic trading, offering a comprehensive suite of products and services designed to elevate your trading success. With our advanced tools and expert guidance, you can unlock the full potential of your trading strategies and achieve consistent results in today&aposs dynamic markets.</p>
                      </div>
                   </div>
                </div>
                <div className="row">
-               <SingleService color="blue-bg-4" img="1" title={<h3 className="">Tailored <br />Precision Strategies</h3>} description={<p>Experience precision in your trading journey with bespoke strategies and screeners crafted specifically to match your unique trading style and indicators, maximizing your potential for success.</p>} />
+                  <SingleService color="blue-bg-4" img="1" title={<h3 className="">Tailored <br />Precision Strategies</h3>} description={<p>Experience precision in your trading journey with bespoke strategies and screeners crafted specifically to match your unique trading style and indicators, maximizing your potential for success.</p>} />
 
-<SingleService color="pink-bg" img="2" title={<h3 className="">Streamlined <br />Efficiency Solutions</h3>} description={<p>Effortlessly navigate the markets with streamlined efficiency as our customized solutions eliminate the need for manual adjustments, saving you time and allowing you to focus on executing winning trades.</p>} />
+                  <SingleService color="pink-bg" img="2" title={<h3 className="">Streamlined <br />Efficiency Solutions</h3>} description={<p>Effortlessly navigate the markets with streamlined efficiency as our customized solutions eliminate the need for manual adjustments, saving you time and allowing you to focus on executing winning trades.</p>} />
 
-<SingleService color="green-bg" img="3" title={<h3 className="">Enhanced <br />Confidence Strategies</h3>} description={<p>Trade with confidence knowing that your strategies and screeners are tailored to your preferences, providing you with a sense of security and assurance as you navigate through various market conditions.</p>} />
+                  <SingleService color="green-bg" img="3" title={<h3 className="">Enhanced <br />Confidence Strategies</h3>} description={<p>Trade with confidence knowing that your strategies and screeners are tailored to your preferences, providing you with a sense of security and assurance as you navigate through various market conditions.</p>} />
 
-<SingleService color="orange-bg" img="4" title={<h3 className="">Optimized <br />Performance Solutions</h3>} description={<p>Achieve optimal performance in your trading endeavors with strategies and screeners finely tuned to your specific objectives, enabling you to capitalize on opportunities and maximize your potential returns.</p>} />
+                  <SingleService color="orange-bg" img="4" title={<h3 className="">Optimized <br />Performance Solutions</h3>} description={<p>Achieve optimal performance in your trading endeavors with strategies and screeners finely tuned to your specific objectives, enabling you to capitalize on opportunities and maximize your potential returns.</p>} />
 
-<SingleService color="blue-bg" img="5" title={<h3 className="">Continuous <br />Adaptation Tools</h3>} description={<p>Stay ahead of the curve with solutions that continuously adapt to your evolving trading style and market dynamics, ensuring that your strategies and screeners remain relevant and effective in today's ever-changing financial landscape.</p>} />
+                  <SingleService color="blue-bg" img="5" title={<h3 className="">Continuous <br />Adaptation Tools</h3>} description={<p>Stay ahead of the curve with solutions that continuously adapt to your evolving trading style and market dynamics, ensuring that your strategies and screeners remain relevant and effective in today&aposs ever-changing financial landscape.</p>} />
 
 
                </div>
